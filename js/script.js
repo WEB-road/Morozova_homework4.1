33ba3f3dbed034979b64b19adad8ed5d943bf928
Sv-mor-79 <morozsv79@gmail.com>
2021-08-03 19:17:04 +0300

    my homework4.1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-const list = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    list.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
-// Inside the Media Query.
+const navLink = document.querySelectorAll(".nav-link");
 
-    .hamburger.active .bar:nth-child(2) {
-        opacity: 0;
-    }
-
-    .hamburger.active .bar:nth-child(1) {
-        transform: translateY(8px) rotate(45deg);
-    }
-
-    .hamburger.active .bar:nth-child(3) {
-        transform: translateY(-8px) rotate(-45deg);
-    }
-
-const link = document.querySelectorAll(".link");
-
-link.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    list.classList.remove("active");
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
 }
